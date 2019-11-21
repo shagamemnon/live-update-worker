a111bc6322052c17db8040bf8fe6ba275b7d9418
shagamemnon <franklin@cloudflare.com>
2019-11-21 11:33:59 -0800

    resolved liquid conflict

--- src/ui.js
+++ src/ui.js
@@ -1,6 +1,6 @@
 export default {
   body (req) {
-    return String.raw`<html>
+    return String.raw`{% raw %}<html>
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
@@ -429,7 +429,7 @@ export default {
 
           </body>
           </html>
-    `
+    {% endraw %}`
   },
 
   render (req) {
